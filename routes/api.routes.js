3bc4067d0e545a3918909a124bb485bf34c961d7
Conchita <codechita@gmail.com>
2021-07-25 19:47:40 +0200

    added search plant api functionality

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const UserModel = require("../models/User.model")
 const PlantModel = require("../models/Plant.model")
+const axios = require('axios');
 
 
 // CLOUDINARY ROUTES //
@@ -10,24 +11,47 @@ const uploader = require('../config/cloudinary.config');
 router.post('/profile/upload', uploader.single("imageUrl"), (req, res, next) => {
     // the uploader.single() callback will send the file to cloudinary and get you and obj with the url in return
     const { email } = req.body
-    
+
     console.log('file is: ', req.file)
-    UserModel.findOneAndUpdate(email, {profileImageUrl: req.file.path}, {new: true})
-    .then((result) => {
-        console.log(result)
-    }).catch((err) => {
-        next(err)
-    });
-    
-    
+    UserModel.findOneAndUpdate(email, { profileImageUrl: req.file.path }, { new: true })
+        .then((result) => {
+            console.log(result)
+        }).catch((err) => {
+            next(err)
+        });
+
+
 
     if (!req.file) {
-      console.log("there was an error uploading the file")
-      next(new Error('No file uploaded!'))
-      return 
+        console.log("there was an error uploading the file")
+        next(new Error('No file uploaded!'))
+        return
     }
 })
-    
-    // You will get the image url in 'req.file.path'
-    // Your code to store your url in your database should be here
+
+router.post('/plant/upload',uploader.single("imageUrl"), async (req, res, next) => {
+    try {
+        console.log('plant file is: ', req.file.path)
+        console.log('type is:', req.body.organ)
+        const { path } =  await req.file
+        const { organ } = await req.body
+        const encoded =  await encodeURIComponent(path);
+        console.log(encoded)
+
+        const response = await axios(`${process.env.PLANT_URL}?api-key=${process.env.PLANT_API_KEY}&images=${encoded}&organs=${organ}`)
+                console.log("CHECKKKKKKKKKK")
+                console.log(response.data.results[0])
+        if (!req.file) {
+            console.log("there was an error uploading the file")
+            next(new Error('No file uploaded!'))
+            return
+        }
+    }
+    catch(err) {
+        console.log(err)
+    };  
+})
+
+// You will get the image url in 'req.file.path'
+// Your code to store your url in your database should be here
 module.exports = router;
