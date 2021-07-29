efbe6fac357fc1743dd92c5a3966cb4ca3541905
Conchita <codechita@gmail.com>
2021-07-29 15:51:09 +0200

    changes format location

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -45,39 +45,43 @@ router.post('/plant/upload', uploader.single("imageUrl"), async (req, res, next)
     };
 })
 
-router.post('/picture/upload', uploader.single("imageUrl"), async (req, res, next) => {
-    try {
-        console.log(req)
-        // const { path } = req.file
+// router.post('/picture/upload', uploader.single("imageUrl"), async (req, res, next) => {
+//     try {
+//         console.log(req)
+//         // const { path } = req.file
         
-        // const { organ } = req.body
-        // const encoded = await encodeURIComponent(path);
-        // const response = await axios(`${process.env.PLANT_URL}?api-key=${process.env.PLANT_API_KEY}&images=${encoded}&organs=${organ}`)
-        // const plantData = {
-        //     plant: response.data.results[0],
-        //     picture: path
-        // }
-        // res.status(200).json(plantData);
-        // if (!req.file) {
-        //     console.log("there was an error uploading the file")
-        //     next(new Error('No file uploaded!'))
-        //     return
-        // }
-    }
-    catch (err) {
-        console.log(err)
-    };
-})
+//         // const { organ } = req.body
+//         // const encoded = await encodeURIComponent(path);
+//         // const response = await axios(`${process.env.PLANT_URL}?api-key=${process.env.PLANT_API_KEY}&images=${encoded}&organs=${organ}`)
+//         // const plantData = {
+//         //     plant: response.data.results[0],
+//         //     picture: path
+//         // }
+//         // res.status(200).json(plantData);
+//         // if (!req.file) {
+//         //     console.log("there was an error uploading the file")
+//         //     next(new Error('No file uploaded!'))
+//         //     return
+//         // }
+//     }
+//     catch (err) {
+//         console.log(err)
+//     };
+// })
 
 
 
 ///////////////// POST INFO ADDPLANT //////////////////
 
 router.post('/plant/add', async (req, res, next) => {
+
     try {
         const { plantImageUrl, displayName, scientificName, commonName, location } = req.body
+        let cordinate = await axios (`https://nominatim.openstreetmap.org/search.php?city=${location}&format=json&accept-language=en`)
+        let city = await cordinate.data[0]
         const { _id: user } = req.session.loggedInUser
-        const result = await PlantModel.create({ plantImageUrl, displayName, scientificName, commonName, location, user })
+        await console.log(city)
+        const result = await PlantModel.create({ plantImageUrl, displayName, scientificName, commonName, location: [+city.lat, +city.lon], user })
         await UserModel.findByIdAndUpdate(user, { $addToSet: { plantsOffered: result._id } }, { new: true })
         res.status(200).json(result);
     }
